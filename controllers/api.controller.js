eec7bfebc2feac564b3189933333c8f386624587
soraly <lequangnam1617@gmail.com>
2022-01-20 14:17:14 +0700

    validate balance

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -15,9 +15,37 @@ class apiController {
       .catch((err) => console.log(err));
   }
 
-
   // [PUT] /api/payment - payment
-  paymentWallet(req, res) {}
+  paymentWallet(req, res) {
+    //Payment cart and minus sodu
+    const id = req.params.id;
+    const amount = req.body.amount;
+    const sql = `select * from public."User" where "id" = ${id}`;
+    db.query(sql)
+      .then((data) => {
+        const balance = data[0].balance;
+        if (balance < 0) {
+          res.json({
+            message: "Số dư không đủ để thanh toán",
+          });
+        } else {
+          const newBalance = balance - amount;
+          if (newBalance < 0) {
+            res.json({
+              message: "Số dư không đủ để thanh toán",
+            });
+          } else {
+            const sql = `update public."User" set "balance" = ${newBalance} where "id" = ${id}`;
+            db.query(sql)
+              .then((data) => {
+                res.json({ data: data, message: "Thanh toán thành công" });
+              })
+              .catch((err) => console.log(err));
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 module.exports = new apiController();
